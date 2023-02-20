cfa218bd812a50a27aa54c28c77e33af7f2be046
unknown <moudi.ld@hotmail.com>
2023-02-20 11:53:27 +0200

    finished controller models view functionality

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    //
+    public function index()
+    {
+        $todos = Todo::all();
+        return view('welcome', compact('todos'));
+    }
+
+    public function store()
+    {
+        $data = request()->validate([
+            'title' => 'required',
+            'description' => 'nullable'
+        ]);
+
+        Todo::create($data);
+
+        return redirect('/');
+    }
+
+    public function update(Todo $todo)
+    {
+        $todo->update(['isDone' => true]);
+
+        return redirect('/');
+    }
+
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+
+        return redirect('/');
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,44 +27,57 @@
     <div class="lg:w-2/4 mx-auto py-8 px-6 mt-6 bg-white rounded-xl">
         <h1 class="font-bold text-5xl text-center mb-8">Todo App</h1>
 
-        <div class="mb-6">
+        <div class="mb-6 h-full">
             <form class="flex flex-col space-y-4" action="/" method="POST">
+                @csrf
                 <input type="text" placeholder="Todo title" name="title"
                     class="py-3 px-4 bg-gray-100 rounded-xl outline-0">
 
-                <textarea name="description" placeholder="Todo Description"
-                    class="py-3 px-4 bg-gray-100 rounded-xl outline-0"></textarea>
+                <textarea name="description" placeholder="Todo Description" class="py-3 px-4 bg-gray-100 rounded-xl outline-0"></textarea>
 
                 <button class="w-28 py-3 px-4 bg-gray-500 text-white rounded-xl font-bold">ADD</button>
             </form>
         </div>
 
         <div class="mt-2">
-            <div class="py-4 flex items-center border-b border-gray-300 px-3">
-                <div class="flex-1 pr-8">
-                    <h3 class="text-lg font-semibold">Title</h3>
-                    <p class="text-gray-500">Description</p>
+
+            @foreach ($todos as $todo)
+                <div @class([
+                    'py-4 px-3 flex items-center border-b border-gray-300 mb-2',
+                    $todo->isDone ? 'bg-green-200' : '',
+                ])>
+                    <div class="flex-1 pr-8">
+                        <h3 class="text-lg font-semibold">{{ $todo->title }}</h3>
+                        <p class="text-gray-500">{{ $todo->description }}</p>
+                    </div>
+
+                    <div class="flex space-x-3">
+                        <form action="/{{ $todo->id }}" method="POST">@csrf
+                            @method('PATCH')
+                            <button class="py-2 px-2 bg-green-400 text-white rounded-xl">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                                </svg>
+                            </button>
+                        </form>
+
+                        <form action="/{{ $todo->id }}" method="POST">@csrf
+                            @method('DELETE')
+                            <button class="py-2 px-2 bg-red-500 text-white rounded-xl">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                </svg>
+                            </button>
+                        </form>
+
+                    </div>
                 </div>
-
-                <div class="flex space-x-3">
-                    <button class="py-2 px-2 bg-green-400 text-white rounded-xl">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-                        </svg>
-                    </button>
-
-                    <button class="py-2 px-2 bg-red-500 text-white rounded-xl">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                        </svg>
-                    </button>
-
-                </div>
-            </div>
+            @endforeach
         </div>
+
     </div>
 </body>
 
